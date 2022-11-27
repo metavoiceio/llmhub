2452d30cdf33c7c9c690389162e53d795552a04a
siddharth sharma <sid3637@gmail.com>
2022-11-27 22:10:47 +0800

    feat: auth0 device flow authentication

--- client/sdk/constants.js
+++ client/sdk/constants.js
@@ -1,8 +1,11 @@
-import path from "path";
-import os from "os";
+const path = require('path');
+const os = require('os');
 
 // export const BASE_URL = "https://api.llmhub.com";
-// export const BASE_URL = "https://458f691f-d0e2-45f7-a1fe-5cdcccf4a867.mock.pstmn.io";
-export const BASE_URL = "http://127.0.0.1:58000";
-// TODO: what's the best to place the below?
-export const TOKEN_FILE_PATH = path.join(os.homedir(), ".llmhub", ".credentials");
+const BASE_URL = "https://458f691f-d0e2-45f7-a1fe-5cdcccf4a867.mock.pstmn.io";
+const TOKEN_FILE_PATH = path.join(os.homedir(), ".llmhub", "credentials");
+
+module.exports = {
+    BASE_URL,
+    TOKEN_FILE_PATH
+}
