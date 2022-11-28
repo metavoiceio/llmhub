f0613c20073def0ab440e9f3b8dbe1017f420261
siddharth sharma <sid3637@gmail.com>
2022-11-28 23:58:42 +0800

    feat: functions

--- client/hub/pages/function.js
+++ client/hub/pages/function.js
@@ -1,16 +1,16 @@
-// represents the authenticated page
-// code out sidebar - move to top on mobile view
-// support functions
-import AuthSideBar from "../components/sidebar";
 import { WORKSPACE_ID } from "../common/constants";
+import AuthSideBar from "../components/sidebar";
 
-export default function Workspace() {
+export default function Function() {
   return (
     <div className="flex flex-col sm:flex-row min-h-screen">
       {/* sidebar */}
       <AuthSideBar workspaceId={WORKSPACE_ID} />
+      {/* topbar */}
+      <div className="sm:hidden block">
+      </div>
       {/* main area */}
-      <div className="h-full flex flex-col flex-1">
+      <div className="h-full flex flex-col overflow-y-auto flex-1">
       </div>
     </div>
   );
