85645906b758b7b26ab609ad15a0f2dc6c04f473
siddharth sharma <sid3637@gmail.com>
2022-11-29 00:18:06 +0800

    feat: add deployments route

---
+++ client/hub/pages/workspace/functions/[:id]/index.js
@@ -0,0 +1,18 @@
+import { WORKSPACE_ID } from "../../../../common/constants";
+import AuthSideBar from "../../../../components/sidebar";
+
+export default function Function() {
+  return (
+    <div className="flex flex-col sm:flex-row min-h-screen">
+      {/* sidebar */}
+      <AuthSideBar workspaceId={WORKSPACE_ID} />
+      {/* topbar */}
+      {/* <div className="sm:hidden block">
+      </div> */}
+      {/* main area */}
+      <div className="h-full flex flex-col overflow-y-auto flex-1">
+        functions page
+      </div>
+    </div>
+  );
+}
