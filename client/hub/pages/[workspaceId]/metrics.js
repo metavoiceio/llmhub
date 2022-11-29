e101bd86a9ebc8c85d4521a3bdd39dcca20985ca
siddharth sharma <sid3637@gmail.com>
2022-11-30 01:41:29 +0800

    ckpt

--- client/hub/pages/[workspaceId]/metrics.js
+++ client/hub/pages/[workspaceId]/metrics.js
@@ -10,7 +10,22 @@ export default function Metrics({ functions }) {
     <div className="flex flex-col sm:flex-row min-h-screen">
       <AuthSideBar workspaceId={workspaceId} functions={functions} />
       <div className="h-full flex flex-col flex-1">
-        Metrics
+        <div className="flex gap-1 items-center">
+          <div className="w-full max-w-[200px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex flex-col items-center pb-10">
+              <h1 className="mt-8 mb-1 text-3xl font-medium text-gray-900 dark:text-white">85%</h1>
+              <span className="text-sm text-gray-500 dark:text-gray-400">Tokens</span>
+              <span className="text-sm text-gray-500 dark:text-gray-400">1,000 / 100,000</span>
+            </div>
+          </div>
+          <div className="w-full max-w-[200px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex flex-col items-center pb-10">
+              <h1 className="mt-8 mb-1 text-3xl font-medium text-gray-900 dark:text-white">850</h1>
+              <span className="text-sm text-gray-500 dark:text-gray-400">API calls</span>
+              <span className="text-sm text-gray-500 dark:text-gray-400">Unlimited</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
