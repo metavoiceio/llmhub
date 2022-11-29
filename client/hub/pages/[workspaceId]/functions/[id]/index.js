36404bd0e425283835e0c95074b0900d8d1c9dfa
siddharth sharma <sid3637@gmail.com>
2022-11-30 00:43:51 +0800

    feat: /functions/[:id] getServerSideProps

--- client/hub/pages/[workspaceId]/functions/[id]/index.js
+++ client/hub/pages/[workspaceId]/functions/[id]/index.js
@@ -1,14 +1,14 @@
 
-import { WORKSPACE_ID } from "../../../../common/constants";
+import { BASE_API_URL } from "../../../../common/constants";
 import AuthSideBar from "../../../../components/sidebar";
 
-export default function Function() {
+export default function Function({workspaceId, functionId, functions}) {
   const renderPlayground = () => {
     return (
       <form>
         <div className="w-full mt-10 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
           <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-            <label for="comment" className="sr-only">Your prompt</label>
+            <label htmlFor="comment" className="sr-only">Your prompt</label>
             <textarea id="comment" rows="4" className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Enter prompt..." required></textarea>
           </div>
           <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
@@ -31,7 +31,8 @@ export default function Function() {
 
   return (
     <div className="flex flex-col sm:flex-row min-h-screen">
-      <AuthSideBar workspaceId={WORKSPACE_ID} />
+      <AuthSideBar workspaceId={workspaceId} functions={functions} />
+
       <div className="h-full flex flex-col overflow-y-auto flex-1 mx-4">
         {/* top bar */}
         <div className="w-full flex item-center gap-2 mt-4">
@@ -41,11 +42,27 @@ export default function Function() {
           <button className="text-sm">Share</button>
         </div>
 
+        <div>in function {functionId}</div>
         {renderPlayground()}
+
         {/* results pane */}
         <div>
         </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  const res = await fetch(`${BASE_API_URL}/functions`, { method: 'GET' });
+  const data = JSON.parse(await res.json());
+
+  // TODO sidroopdaska: fetch the function details
+  return {
+    props: {
+      workspaceId: params.workspaceId,
+      functionId: params.id,
+      functions: data
+    }
+  }
 }
