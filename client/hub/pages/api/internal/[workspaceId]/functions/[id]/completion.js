d0c6290e1d8f44a3ece83c9a7b471d9270979c2c
siddharth sharma <sid3637@gmail.com>
2022-12-01 02:59:53 +0800

    feat: write experiment to db

--- client/hub/pages/api/internal/[workspaceId]/functions/[id]/completion.js
+++ client/hub/pages/api/internal/[workspaceId]/functions/[id]/completion.js
@@ -1,20 +1,51 @@
+import { supabase } from '../../../../../../common/supabase';
+
 function mockCompletionApiCall() {
   return new Promise((resolve, reject) => {
     setTimeout(() => resolve({
-      output: 'This is a generated output by a LLM'
+      output: 'This is a generated output by a LLM',
+      num_tokens: 2048,
+      duration_s: 2.4
     }), 2000);
   });
 }
 
 export default async function handler(req, res) {
+  const { workspaceId, id } = req.query
+
   if (req.method !== 'POST') return res.status(500).json({ error: 'Invalid request' });
 
-  console.log(req.body);
+  const { prompt, input, model, config } = req.body;
 
   // call completion API
-  await mockCompletionApiCall();
+  const { output, num_tokens, duration_s } = await mockCompletionApiCall();
 
-  // update db
+  // insert to experiments
+  const { data, error } = await supabase
+    .from('experiments')
+    .insert([
+      {
+        prompt,
+        model,
+        config,
+        input,
+        output,
+        num_tokens,
+        duration_s,
+        function_id: id
+      },
+    ])
+    .select();
+
+  if (error) return res.status(500).json({ error });
+
+  // update functions
+  const functionsOp = await supabase
+    .from('functions')
+    .update({ 'current_experiment_id': data[0].id })
+    .eq('id', id)
+
+  if (functionsOp.error) return res.status(500).json({ error });
 
   // return to frontend
   return res.status(201).json({ status: 'success' })
