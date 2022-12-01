c15e5887d0fcf6b4698e942b352feecb1816f64d
siddharth sharma <sid3637@gmail.com>
2022-12-01 17:18:23 +0800

    feat: deployments getServerSideProps

--- client/hub/pages/[workspaceId]/functions/[id]/deployments.js
+++ client/hub/pages/[workspaceId]/functions/[id]/deployments.js
@@ -1,17 +1,24 @@
 import { useRouter } from "next/router";
-import { getFunctions } from "../../../../common/supabase";
+import { getFunctions, supabase } from "../../../../common/supabase";
 import AuthSideBar from "../../../../components/sidebar";
 
-export default function Function({ functions }) {
+// TODO sidroopdaska: error handling
+export default function Function({ functions, deployments }) {
   const router = useRouter();
   const { workspaceId, id } = router.query;
+
+  const renderDeployments = () => {
+    return <p>have {deployments.length} deployments to render</p>
+  }
 
   return (
     <div className="flex flex-col sm:flex-row min-h-screen">
       <AuthSideBar workspaceId={workspaceId} functions={functions} />
 
       <div className="h-full flex flex-col overflow-y-auto flex-1">
-        Deployments page
+        {
+          deployments.length === 0 ? <p>No deployments have been made.</p> : renderDeployments()
+        }
       </div>
     </div>
   );
@@ -20,9 +27,19 @@ export default function Function({ functions }) {
 export async function getServerSideProps({ params }) {
   let { workspaceId, id } = params;
 
+  // fetch deployments for function
+  const { data: deployments, error } = await supabase
+    .from('deployments')
+    .select('*')
+    .eq('function_id', id)
+    .order('created_at', { ascending: false });
+
+  console.log(error);
+
   return {
     props: {
       functions: await getFunctions(workspaceId),
+      deployments
     }
   };
 }
