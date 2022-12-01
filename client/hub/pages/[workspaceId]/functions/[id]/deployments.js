4bf304fca51d725f2ed0f1fc4f2ff3a7a4a7b5c2
siddharth sharma <sid3637@gmail.com>
2022-12-01 17:07:06 +0800

    fix: deployments page

--- client/hub/pages/[workspaceId]/functions/[id]/deployments.js
+++ client/hub/pages/[workspaceId]/functions/[id]/deployments.js
@@ -1,14 +1,28 @@
-import { WORKSPACE_ID } from "../../../../common/constants";
+import { useRouter } from "next/router";
+import { getFunctions } from "../../../../common/supabase";
 import AuthSideBar from "../../../../components/sidebar";
 
-export default function Function() {
+export default function Function({ functions }) {
+  const router = useRouter();
+  const { workspaceId, id } = router.query;
+
   return (
     <div className="flex flex-col sm:flex-row min-h-screen">
-      <AuthSideBar workspaceId={WORKSPACE_ID} />
+      <AuthSideBar workspaceId={workspaceId} functions={functions} />
 
       <div className="h-full flex flex-col overflow-y-auto flex-1">
         Deployments page
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  let { workspaceId, id } = params;
+
+  return {
+    props: {
+      functions: await getFunctions(workspaceId),
+    }
+  };
 }
