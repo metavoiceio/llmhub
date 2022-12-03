5f900b8c3557727a991df696fa61fe18123d05cf
Vatsal Aggarwal <vatsalaggarwal@gmail.com>
2022-12-04 01:45:34 +0800

    Vatsal/server completion api (#19)
    * remove python db ops
    
    * change completions api to conform to frontend

--- server/hub/hub_server.py
+++ server/hub/hub_server.py
@@ -1,26 +1,16 @@
+from typing import Dict
+
 import fastapi
 import fastapi.middleware.cors
 import uvicorn
 
-from server.hub.db_ops import apps, prompts, stars, usage, users
 from server.providers.openai import OpenAI
 
-# TODO: ability to swap providers easily?
-# TODO: ability to swap models easily?
-# TODO: best way to organise this so that users can issue generation against
-#       any provider and any params easily?
 OPENAI_API_KEY = "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"
-# llm_provider = OpenAI(OPENAI_API_KEY, "code-davinci-002")
-# llm_provider = OpenAI(OPENAI_API_KEY, "text-davinci-002")
-llm_provider = OpenAI(OPENAI_API_KEY, "text-ada-001")
+llm_provider = OpenAI(OPENAI_API_KEY)
 
 ## Setup FastAPI server.
 app = fastapi.FastAPI()
-app.include_router(apps.router)
-app.include_router(prompts.router)
-app.include_router(users.router)
-app.include_router(usage.router)
-app.include_router(stars.router)
 app.add_middleware(
     fastapi.middleware.cors.CORSMiddleware,
     allow_origins=["*", "http://localhost:3000"],
@@ -29,11 +19,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# TODO: convert to a database
-llmhub_prompt_id_to_prompt = {
-    "test": "This is a test prompt.",
-}
-
 
 @app.get("/")
 async def health_check():
@@ -41,16 +26,10 @@ async def health_check():
 
 
 @app.get("/completion")
-async def get_completion(input: str, llmhub_prompt_id: str):
-    # TODO: @sidroopdaska auth
-    default_prompt = llmhub_prompt_id_to_prompt[llmhub_prompt_id]
-    prompt = default_prompt + input
-    # TODO: get num_tokens as well!
-    answer = llm_provider(prompt)
+async def get_completion(prompt: str, input: str, config: Dict[str, str]):
+    output, num_tokens, duration_s = llm_provider(prompt, input, config)
 
-    # append to usage!
-
-    return {"completion": answer}
+    return {"output": output, "num_tokens": num_tokens, "duration_s": duration_s}
 
 
 if __name__ == "__main__":
--- tests/integration_tests/llms/providers/test_openai.py
+++ tests/integration_tests/llms/providers/test_openai.py
@@ -1,1 +1,15 @@
 from server.providers import OpenAI
+
+openai = OpenAI("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")
+print(
+    openai(
+        "This is a test prompt.", "", {"engine": "text-ada-001", "max_tokens": 10, "user": "test"}
+    )
+)
+print(
+    openai(
+        "This is a test prompt.",
+        "",
+        {"engine": "text-davinci-003", "max_tokens": 10, "user": "test"},
+    )
+)
