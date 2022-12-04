130b8278c377928fed301156c1c23719a2367ebc
Vatsal Aggarwal <vatsalaggarwal@gmail.com>
2022-12-05 03:01:17 +0800

    fix cli (#24)
    * fix cli
    
    * fix eol

--- client/sdk/appAuth.js
+++ client/sdk/appAuth.js
@@ -1,24 +1,15 @@
-import fs from "fs-extra";
-import { TOKEN_FILE_PATH } from "./constants.js";
+const fs = require("fs-extra");
+const { TOKEN_FILE_PATH } = require("./constants");
 
 // USED BY APP
 
-export const get_saved_token = async () => {
+const get_token = async () => {
     if (fs.existsSync(TOKEN_FILE_PATH) && fs.statSync(TOKEN_FILE_PATH).isFile()) {
       const fileContent = await fs.readFile(TOKEN_FILE_PATH, "utf-8");
-      const [name, value] = fileContent?.split("=") || [];
-      if (name === "LLMHUB_USER_ID") {
-        return value.trim();
-      }
+      const { token } = JSON.parse(fileContent);
+      return token;
     }
-    return null;
+    throw new Error("Please set up your LLMHub token using `llmhub auth`!");
   }
 
-
-export const get_token = async () => {
-  let token = await get_saved_token();
-  if (token) {
-    return token;
-  }
-  throw new Error("Please set up your LLMHub token using `llmhub-auth`!");
-}
+module.exports = { get_token };
--- client/sdk/test.js
+++ client/sdk/test.js
@@ -1,3 +1,8 @@
-import LLMHub from "./main.js";
-const llmhub = new LLMHub();
-await llmhub.run("This is a test prompt");
+const { LLMHub } = require("./main");
+
+const llmhub = new LLMHub(1, 3);
+llmhub.run("This is a test prompt").then(
+    (output) => {
+        console.log(output);
+    }
+);
