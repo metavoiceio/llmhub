b05cf6b696fb501163caee4954085a4015b8e0d2
Vatsal <vatsalaggarwal@gmail.com>
2022-12-04 19:34:05 +0000

    feat: use llmhub share link inside cli.

--- client/sdk/test.js
+++ client/sdk/test.js
@@ -1,6 +1,7 @@
 const { LLMHub } = require("./main");
 
-const llmhub = new LLMHub(1, 3);
+// const llmhub = new LLMHub(1, 3);
+const llmhub = new LLMHub("https://www.llmhub.com/2/functions/5/share")
 llmhub.run("This is a test prompt").then(
     (output) => {
         console.log(output);
