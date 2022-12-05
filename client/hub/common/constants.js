229658546cad13edeac97e3640488019fda6f0bc
Vatsal <vatsalaggarwal@gmail.com>
2022-12-05 16:20:38 +0000

    Enable all relevant OpenAI models.

--- client/hub/common/constants.js
+++ client/hub/common/constants.js
@@ -16,7 +16,35 @@ export const INITIAL_MODEL_CONFIGS = {
     maximumLength: 256,
     stopSequences: "",
   },
-  'text-davinci-002': {
+  'code-davinci-002': {
+    temperature: 0.7,
+    presencePenalty: 0.0,
+    frequencyPenalty: 0.0,
+    maximumLength: 256,
+    stopSequences: "",
+  },
+  'code-cushman-001': {
+    temperature: 0.7,
+    presencePenalty: 0.0,
+    frequencyPenalty: 0.0,
+    maximumLength: 256,
+    stopSequences: "",
+  },
+  'text-curie-001': {
+    temperature: 0.7,
+    presencePenalty: 0.0,
+    frequencyPenalty: 0.0,
+    maximumLength: 256,
+    stopSequences: "",
+  },
+  'text-babbage-001': {
+    temperature: 0.7,
+    presencePenalty: 0.0,
+    frequencyPenalty: 0.0,
+    maximumLength: 256,
+    stopSequences: "",
+  },
+  'text-ada-001': {
     temperature: 0.7,
     presencePenalty: 0.0,
     frequencyPenalty: 0.0,
@@ -25,9 +53,12 @@ export const INITIAL_MODEL_CONFIGS = {
   }
 };
 
+const TOKEN_MAX_LENGTHS = [4000, 8000, 2048, 2048, 2048, 2048];
+
 export const SUPPORTED_MODELS = Object.keys(INITIAL_MODEL_CONFIGS);
-export const RENDER_CONFIG_INDEX = {
-  'text-davinci-003': {
+export const RENDER_CONFIG_INDEX = {};
+Object.keys(INITIAL_MODEL_CONFIGS).forEach((key, index) => {
+  RENDER_CONFIG_INDEX[key] = {
     temperature: {
       min: 0.0,
       max: 1.0,
@@ -54,42 +85,8 @@ export const RENDER_CONFIG_INDEX = {
     },
     maximumLength: {
       min: 1,
-      max: 4000,
-      helper: '',
-      friendlyName: 'Maximum length',
-      default: 256,
-      step: 10
-    },
-    stopSequences: {}
-  },
-  'text-davinci-002': {
-    temperature: {
-      min: 0.0,
-      max: 1.0,
-      default: 0.7,
-      helper: 'Rigid: 0.0, Moderate: 0.5 & Creative 1.0',
-      friendlyName: 'Temperature',
-      step: 0.1
-    },
-    frequencyPenalty: {
-      min: 0.0,
-      max: 1.0,
-      helper: '',
-      default: 0.0,
-      friendlyName: 'Frequency Penalty',
-      step: 0.1
-    },
-    presencePenalty: {
-      min: 0.0,
-      max: 1.0,
-      helper: '',
-      default: 0.0,
-      friendlyName: 'Presence Penalty',
-      step: 0.1
-    },
-    maximumLength: {
-      min: 1,
-      max: 4000,
+      // TODO: Note: this is the MAX length of the prompt+input, whereas this is only accounting for the input.
+      max: TOKEN_MAX_LENGTHS[index],
       helper: '',
       friendlyName: 'Maximum length',
       default: 256,
@@ -97,7 +94,7 @@ export const RENDER_CONFIG_INDEX = {
     },
     stopSequences: {}
   }
-}
+});
 
 // TODO sidroopdaska: replace with above
 export const ATTR_FRIENDLY_NAME_INDEX = {
