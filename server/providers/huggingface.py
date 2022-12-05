1499691c59f47766fa0d85abdf6d884c9e5ce21d
Vatsal <vatsal@themetavoice.xyz>
2022-12-05 17:02:52 +0000

    fix: huggingface provider bug.

--- server/providers/huggingface.py
+++ server/providers/huggingface.py
@@ -1,5 +1,3 @@
-# GPT-J
-# T5-XXL?
 import time
 from typing import Dict
 
@@ -9,7 +7,6 @@ from server.providers.base import LLM
 
 API_TOKEN = "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"
 
-# API_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neox-20b"
 API_URL = "https://dcks6mrqe7w3t87f.us-east-1.aws.endpoints.huggingface.cloud"
 headers = {
     "Authorization": f"Bearer {API_TOKEN}",
@@ -32,12 +29,4 @@ class HuggingFace(LLM):
         )
         end_time = time.time()
 
-        return response.json()["generated_text"], 0, start_time - end_time
-
-
-# output = query("Can you please let us know more details about your ")
-# print(output)
-# output = query("Please answer the following question. What is the boiling point of Nitrogen?")
-# print(output)
-# output = query("Translate to German: My name is Arthur")
-# print(output)
+        return response.json()[0]["generated_text"], 0, end_time - start_time
