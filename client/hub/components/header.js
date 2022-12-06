d19cb0f3f7092cd846665f8ce47267ab1a1d41de
Vatsal <vatsalaggarwal@gmail.com>
2022-12-06 14:40:30 +0000

    feat: edit header

--- client/hub/components/header.js
+++ client/hub/components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <Head>
             <title>LLMHub</title>
-            <meta name="description" content="LLMHub is where people build software on top of Large Language Models (LLMs). People use LLMHub to discover, fork, and contribute to prompts." />
+            <meta name="description" content="LLMHub is where people build software on top of Large Language Models (LLMs). People use LLMHub to improve, discover, fork, and contribute to prompts." />
             <link rel="icon" href="/favicon.ico" />
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
         </Head>
