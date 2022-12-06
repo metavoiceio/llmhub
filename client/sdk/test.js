72bd1ade00e580868f080250b6202a18570a0655
Vatsal <vatsalaggarwal@gmail.com>
2022-12-06 15:13:16 +0000

    fix/js-sdk: fix bug

--- client/sdk/test.js
+++ client/sdk/test.js
@@ -1,8 +1,10 @@
-const { LLMHub } = require("./main");
+const LLMHub = require("./main");
 
 const llmhub = new LLMHub("https://www.llmhub.com/2/functions/5/share")
-llmhub.run("This is a test prompt").then(
-    (output) => {
-        console.log(output);
+llmhub.run({
+    input: "This is a test prompt"
+}).then(
+    (response) => {
+        console.log(response);
     }
 );
