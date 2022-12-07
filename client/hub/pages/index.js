5f51ddd75d89a7cac5cc05df466f9a384b4a284a
siddharth sharma <sid3637@gmail.com>
2022-12-07 20:58:15 +0000

    feat: landing points

--- client/hub/pages/index.js
+++ client/hub/pages/index.js
@@ -24,61 +24,27 @@ export default function Home() {
         {/* <h3 className={`${styles.hubIntro} mt-2 mb-2`}>Large Language Model-based applications are hard to build and hard to use.</h3> */}
         <h1 className={`text-5xl mt-[6rem] mb-5`}>The best way to <span className="text-green-400">discover,</span></h1>
         <h1 className={`text-5xl text-green-400`}>design, deploy and use LLM Apps</h1>
-        <div className='flex gap-5 mt-[6rem]'>
-          <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400 text-center">Author</h5>
-            <ul role="list" className="space-y-5 my-7">
-              {
-                [
-                  "1-time 1-click setup",
-                  "Best way to use open-source projects that use LLMs."
-                ].map((point, idx) => {
-                  return (
-                    <li className="flex space-x-3" key={idx}>
-                      <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                      <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{point}</span>
-                    </li>
-                  )
-                })
-              }
-            </ul>
-          </div>
-
-          <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400 text-center">Open-source Contributor</h5>
-            <ul role="list" className="space-y-5 my-7">
-              {
-                [
-                  "1-time 1-click setup",
-                  "Best way to use open-source projects that use LLMs."
-                ].map((point, idx) => {
-                  return (
-                    <li className="flex space-x-3" key={idx}>
-                      <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                      <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{point}</span>
-                    </li>
-                  )
-                })
-              }
-            </ul>
-          </div>
-          <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400 text-center">End User</h5>
-            <ul role="list" className="space-y-5 my-7">
-              {
-                [
-                  "1-time 1-click setup",
-                  "Best way to use open-source projects that use LLMs."
-                ].map((point, idx) => {
-                  return (
-                    <li className="flex space-x-3" key={idx}>
-                      <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                      <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{point}</span>
-                    </li>
-                  )
-                })
-              }
-            </ul>
+        <div className="py-16 px-4 sm:px-6 xl:py-24 xl:px-8">
+          <div className="sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 xl:grid-cols-3 xl:gap-x-8 flex items-center">
+            {[
+              "Integrate ANY LLM in 3-lines with a standardised API",
+              "Deploy in 1-click",
+              "Allow users to use your app in 1-click",
+              "Monitor and improve your prompts",
+              "Discover new prompts easily, and showcase your work",
+              "Contribute to popular prompts, and improve your skills"
+            ].map((statement, idx) => {
+              return (
+                <div className="relative" key={idx}>
+                  <dt>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" className="absolute h-6 w-6 text-green-500"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path></svg>
+                    <p className="flex flex-row ml-9 text-lg font-medium leading-6 text-gray-900 items-center">
+                      <span>{statement}</span>
+                    </p>
+                  </dt>
+                </div>
+              )
+            })}
           </div>
         </div>
       </main>
@@ -88,7 +54,7 @@ export default function Home() {
 }
 
 
-// - Integrate ANY LLM in 3-lines with a standardised API.
+// - Integrate ANY LLM in 3-lines with a standardised API
 // - Deploy in 1-click.
 // - Allow users to use your app in 1-click.
 
