747aa422269adcc6c768af13c02f096e1b2a06a3
siddharth sharma <sid3637@gmail.com>
2022-12-07 13:28:22 +0000

    feat: active deployments

--- client/hub/pages/[workspaceId]/functions/[id]/deployments.js
+++ client/hub/pages/[workspaceId]/functions/[id]/deployments.js
@@ -1,3 +1,4 @@
+import { Accordion } from "flowbite-react";
 import { useRouter } from "next/router";
 import { ATTR_FRIENDLY_NAME_INDEX } from "../../../../common/constants";
 import { getFunctions, supabase } from "../../../../common/supabase";
@@ -14,7 +15,28 @@ export default function Function({ functions, deployments, currentDeploymentId }
         <div>
           <span className="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-1 rounded dark:bg-pink-200 dark:text-pink-900">PAST</span>
         </div>
-        
+        <Accordion alwaysOpen={false}>
+          <Accordion.Panel>
+            <Accordion.Title>
+              What is Flowbite?
+            </Accordion.Title>
+            <Accordion.Content>
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                Check out this guide to learn how to{' '}
+                <a
+                  href="https://flowbite.com/docs/getting-started/introduction/"
+                  className="text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  get started
+                </a>
+                {' '}and start developing websites even faster with components on top of Tailwind CSS.
+              </p>
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
       </>
     )
   }
@@ -24,34 +46,39 @@ export default function Function({ functions, deployments, currentDeploymentId }
     const experiment = currentDeployment.experiments;
     return (
       <div className="p-4 mt-16 overflow-y-auto flex flex-col gap-5">
-        {/* active deployment */}
         <div>
           <span className="bg-green-100 text-green-500 text-xs font-medium mr-2 px-2.5 py-1 rounded dark:bg-green-200 dark:text-green-900">
             ACTIVE
           </span>
         </div>
-        <div>
-          <div className="mb-2 pl-1 text-sm text-gray-700 dark:text-white">{moment(currentDeployment.created_at).format("MMMM Do YYYY, h:mm:ss")}</div>
-          <div className="p-4 flex bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <textarea readOnly className="flex-1 text-sm font-normal text-gray-900 dark:text-gray-400 border-none border-transparent focus:border-transparent focus:ring-0">{experiment.prompt}</textarea>
-            <div className="w-[20rem] max-md:w-[12rem] border-l flex flex-col pl-8 pr-8">
-              <div className="flex items-center justify-between">
-                <div className="text-xs text-gray-800">Model</div>
-                <div className="text-sm">{experiment.model}</div>
+        <Accordion alwaysOpen={true} flush>
+          <Accordion.Panel>
+            <Accordion.Title className="text-sm">
+              {moment(currentDeployment.created_at).format("MMMM Do YYYY, h:mm:ss")}
+            </Accordion.Title>
+            <Accordion.Content>
+              <div className="-ml-2 flex">
+                <textarea readOnly className="flex-1 text-sm font-normal text-gray-900 dark:text-gray-400 border-none border-transparent focus:border-transparent focus:ring-0">{experiment.prompt}</textarea>
+                <div className="w-[20rem] max-md:w-[12rem] border-l flex flex-col pl-8 pr-8">
+                  <div className="flex items-center justify-between">
+                    <div className="text-xs text-gray-800">Model</div>
+                    <div className="text-sm">{experiment.model}</div>
+                  </div>
+                  {
+                    Object.entries(experiment.config).map(([key, value], idx) => {
+                      return (
+                        <div className="flex items-center justify-between" key={idx}>
+                          <div className="text-xs text-gray-800">{ATTR_FRIENDLY_NAME_INDEX[key]}</div>
+                          <div className="text-sm">{value}</div>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
               </div>
-              {
-                Object.entries(experiment.config).map(([key, value], idx) => {
-                  return (
-                    <div className="flex items-center justify-between" key={idx}>
-                      <div className="text-xs text-gray-800">{ATTR_FRIENDLY_NAME_INDEX[key]}</div>
-                      <div className="text-sm">{value}</div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-          </div>
-        </div>
+            </Accordion.Content>
+          </Accordion.Panel>
+        </Accordion>
         {renderPastDeployments()}
       </div>
     )
