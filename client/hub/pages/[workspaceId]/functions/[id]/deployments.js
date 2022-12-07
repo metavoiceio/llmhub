8b23b12b90537989743f1c4ea4c0b86096ed3a2a
siddharth sharma <sid3637@gmail.com>
2022-12-07 13:39:12 +0000

    feat: render past deployments

--- client/hub/pages/[workspaceId]/functions/[id]/deployments.js
+++ client/hub/pages/[workspaceId]/functions/[id]/deployments.js
@@ -9,41 +9,64 @@ export default function Function({ functions, deployments, currentDeploymentId }
   const router = useRouter();
   const { workspaceId, id } = router.query;
 
-  const renderPastDeployments = () => {
+  const renderDeploymentContent = (prompt, model, config) => {
+    return (
+      <div className="-ml-2 flex">
+        <textarea readOnly className="flex-1 text-sm font-normal text-gray-900 dark:text-gray-400 border-none border-transparent focus:border-transparent focus:ring-0">
+          {prompt}
+        </textarea>
+        <div className="w-[20rem] max-md:w-[12rem] border-l flex flex-col pl-8 pr-8">
+          <div className="flex items-center justify-between">
+            <div className="text-xs text-gray-800">Model</div>
+            <div className="text-sm">{model}</div>
+          </div>
+          {
+            Object.entries(config).map(([key, value], idx) => {
+              return (
+                <div className="flex items-center justify-between" key={idx}>
+                  <div className="text-xs text-gray-800">{ATTR_FRIENDLY_NAME_INDEX[key]}</div>
+                  <div className="text-sm">{value}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    )
+  }
+
+  const renderDeployments = (deployments) => {
     return (
       <>
-        <div>
-          <span className="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-1 rounded dark:bg-pink-200 dark:text-pink-900">PAST</span>
-        </div>
-        <Accordion alwaysOpen={false}>
-          <Accordion.Panel>
-            <Accordion.Title>
-              What is Flowbite?
-            </Accordion.Title>
-            <Accordion.Content>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
-              </p>
-              <p className="text-gray-500 dark:text-gray-400">
-                Check out this guide to learn how to{' '}
-                <a
-                  href="https://flowbite.com/docs/getting-started/introduction/"
-                  className="text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  get started
-                </a>
-                {' '}and start developing websites even faster with components on top of Tailwind CSS.
-              </p>
-            </Accordion.Content>
-          </Accordion.Panel>
+        <Accordion flush>
+          {
+            deployments.map((deployment, idx) => {
+              return (
+                <Accordion.Panel key={idx}>
+                  <Accordion.Title>
+                    {moment(deployment.created_at).format("MMMM Do YYYY, h:mm:ss")}
+                  </Accordion.Title>
+                  <Accordion.Content>
+                    {
+                      renderDeploymentContent(
+                        deployment.experiments.prompt,
+                        deployment.experiments.model,
+                        deployment.experiments.config
+                      )
+                    }
+                  </Accordion.Content>
+                </Accordion.Panel>
+              )
+            })
+          }
         </Accordion>
       </>
     )
   }
 
-  const renderDeployments = () => {
-    const currentDeployment = deployments.filter(d => d.id === currentDeploymentId)[0];
-    const experiment = currentDeployment.experiments;
+  const render = () => {
+    const currentDeployments = deployments.filter(d => d.id === currentDeploymentId);
+    const pastDeployments = deployments.filter(d => d.id !== currentDeploymentId);
     return (
       <div className="p-4 mt-16 overflow-y-auto flex flex-col gap-5">
         <div>
@@ -51,35 +74,13 @@ export default function Function({ functions, deployments, currentDeploymentId }
             ACTIVE
           </span>
         </div>
-        <Accordion alwaysOpen={true} flush>
-          <Accordion.Panel>
-            <Accordion.Title className="text-sm">
-              {moment(currentDeployment.created_at).format("MMMM Do YYYY, h:mm:ss")}
-            </Accordion.Title>
-            <Accordion.Content>
-              <div className="-ml-2 flex">
-                <textarea readOnly className="flex-1 text-sm font-normal text-gray-900 dark:text-gray-400 border-none border-transparent focus:border-transparent focus:ring-0">{experiment.prompt}</textarea>
-                <div className="w-[20rem] max-md:w-[12rem] border-l flex flex-col pl-8 pr-8">
-                  <div className="flex items-center justify-between">
-                    <div className="text-xs text-gray-800">Model</div>
-                    <div className="text-sm">{experiment.model}</div>
-                  </div>
-                  {
-                    Object.entries(experiment.config).map(([key, value], idx) => {
-                      return (
-                        <div className="flex items-center justify-between" key={idx}>
-                          <div className="text-xs text-gray-800">{ATTR_FRIENDLY_NAME_INDEX[key]}</div>
-                          <div className="text-sm">{value}</div>
-                        </div>
-                      )
-                    })
-                  }
-                </div>
-              </div>
-            </Accordion.Content>
-          </Accordion.Panel>
-        </Accordion>
-        {renderPastDeployments()}
+        {renderDeployments(currentDeployments)}
+        <div>
+          <span className="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-1 rounded dark:bg-pink-200 dark:text-pink-900">
+            PAST
+          </span>
+        </div>
+        {renderDeployments(pastDeployments)}
       </div>
     )
   }
@@ -87,10 +88,9 @@ export default function Function({ functions, deployments, currentDeploymentId }
   return (
     <div className="flex flex-col sm:flex-row min-h-screen">
       <AuthSideBar workspaceId={workspaceId} functions={functions} />
-      {/* TODO: render header */}
       <div className="h-full flex flex-col overflow-y-auto flex-1">
         {
-          deployments.length === 0 ? <p>No deployments have been made.</p> : renderDeployments()
+          deployments.length === 0 ? <p>No deployments have been made.</p> : render()
         }
       </div>
     </div>
