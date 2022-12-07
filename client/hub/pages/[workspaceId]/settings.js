dd581a9b8643855ce0b01e818c59c47ab32bf6f0
siddharth sharma <sid3637@gmail.com>
2022-12-07 14:36:50 +0000

    feat: settings layout

--- client/hub/pages/[workspaceId]/settings.js
+++ client/hub/pages/[workspaceId]/settings.js
@@ -10,26 +10,26 @@ export default function Settings({ functions, workspace }) {
 
   const renderTokenUsage = () => {
     return (
-      <>
+      <div className="grow max-w-lg">
         <div className="flex justify-between mb-1">
           <span className="text-base font-medium text-green-700 dark:text-white">Token Usage</span>
           <span className="text-sm font-medium text-green-700 dark:text-white">{workspace.usage_tokens}/250k</span>
         </div>
         <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-          <div className="bg-green-500 h-2.5 rounded-full" style={{ width: `${workspace.usage_tokens/250000 * 100}%` }}></div>
+          <div className="bg-green-500 h-2.5 rounded-full" style={{ width: `${workspace.usage_tokens / 250000 * 100}%` }}></div>
         </div>
-      </>
+      </div>
     )
   }
 
   return (
     <div className="flex flex-col sm:flex-row min-h-screen">
       <AuthSideBar workspaceId={workspaceId} functions={functions} />
-      <div className="h-full flex flex-col flex-1 p-4">
+      <div className="h-screen flex-1 py-4 flex flex-col px-8">
         <h1 className="font-medium text-xl mb-20">Settings</h1>
         <div className="mb-4">Plan: Free</div>
         {renderTokenUsage()}
-        <div className="mt-20">
+        <div>
           <Button
             size="sm"
             color={"failure"}
