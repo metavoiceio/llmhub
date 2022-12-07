c162f43f54e56ad8adaca6e0b2cde96c95e914fa
siddharth sharma <sid3637@gmail.com>
2022-12-07 20:17:17 +0000

    feat: landing page cards

--- client/hub/pages/index.js
+++ client/hub/pages/index.js
@@ -1,5 +1,4 @@
 import Footer from "../components/footer"
-import Header from "../components/header"
 import UnAuthNavBar from "../components/unAuthNavBar"
 import styles from '../styles/Home.module.css'
 
@@ -22,36 +21,93 @@ export default function Home() {
     <div className={styles.container}>
       <UnAuthNavBar />
       <main className={styles.main}>
-        <h1 className={styles.title}>LLMHub</h1>
-        <h3 className={`${styles.hubIntro} mt-2 mb-2`}>Large Language Model-based applications are hard to build and hard to use.</h3>
-        <h3 className={`${styles.hubIntro} mb-5`}>LLMHub is the best place on the 👨‍💻 to discover, create, ship, and use prompts.</h3>
-        {/* <h3 className={styles.hubIntro}> LLM-based apps are programmed in a completely new way.   </h3>
-        <h3 className={`${styles.hubIntro} mb-5`}> We fix all the problems with building apps on top of them, and with using those apps.</h3> */}
-        <div className={styles.valuePropsContainer}>
-        {renderValuePropDiv("App Users", [
-          "💆 Ease of use: 1-time 1-click setup",
-          "🙌 No need to setup APIs with different providers",
-        ])}
-        {renderValuePropDiv("Developers", [
-          "🍳 Easy deployment",
-          "😌 Standardised API across all providers, including open-source models (already deployed)!",
-          "💪 Showcase your work easily",
-          "💻 Visibility into the behavior of prompt",
-          "📻 Easy finetuning",
-          "🔧 Tools to improve latency, speed, and accuracy of the LLM request"
-        ]
-        )}
-        {renderValuePropDiv("Contributors", [
-          "🕵️ Find popular prompts to contribute to easily",
-          "🛠️ Tools to showcase improvements to prompts",
-          "🤔 Standardisation of evaluation",
-          "🧘‍♂️ Focus on improving prompts, not on setting up.",
-          "🔥 Showcase your contributions on the hub",
-        ]
-        )}
-      </div>
+        {/* <h3 className={`${styles.hubIntro} mt-2 mb-2`}>Large Language Model-based applications are hard to build and hard to use.</h3> */}
+        <h1 className={`text-5xl mt-[6rem] mb-5`}>The best way to <span className="text-green-400">discover,</span></h1>
+        <h1 className={`text-5xl text-green-400`}>design, deploy and use LLM Apps</h1>
+        <div className='flex gap-5 mt-[6rem]'>
+          <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400 text-center">Author</h5>
+            <ul role="list" className="space-y-5 my-7">
+              {
+                [
+                  "1-time 1-click setup",
+                  "Best way to use open-source projects that use LLMs."
+                ].map((point, idx) => {
+                  return (
+                    <li className="flex space-x-3" key={idx}>
+                      <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                      <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{point}</span>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+
+          <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400 text-center">Open-source Contributor</h5>
+            <ul role="list" className="space-y-5 my-7">
+              {
+                [
+                  "1-time 1-click setup",
+                  "Best way to use open-source projects that use LLMs."
+                ].map((point, idx) => {
+                  return (
+                    <li className="flex space-x-3" key={idx}>
+                      <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                      <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{point}</span>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+          <div className="w-full max-w-sm p-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400 text-center">End User</h5>
+            <ul role="list" className="space-y-5 my-7">
+              {
+                [
+                  "1-time 1-click setup",
+                  "Best way to use open-source projects that use LLMs."
+                ].map((point, idx) => {
+                  return (
+                    <li className="flex space-x-3" key={idx}>
+                      <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                      <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">{point}</span>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
       </main>
       <Footer />
     </div>
   );
 }
+
+
+// {renderValuePropDiv("App Users", [
+//   "💆 Ease of use: 1-time 1-click setup",
+//   "🙌 No need to setup APIs with different providers",
+// ])}
+// {renderValuePropDiv("Developers", [
+//   "🍳 Easy deployment",
+//   "😌 Standardised API across all providers, including open-source models (already deployed)!",
+//   "💪 Showcase your work easily",
+//   "💻 Visibility into the behavior of prompt",
+//   "📻 Easy finetuning",
+//   "🔧 Tools to improve latency, speed, and accuracy of the LLM request"
+// ]
+// )}
+// {renderValuePropDiv("Contributors", [
+//   "🕵️ Find popular prompts to contribute to easily",
+//   "🛠️ Tools to showcase improvements to prompts",
+//   "🤔 Standardisation of evaluation",
+//   "🧘‍♂️ Focus on improving prompts, not on setting up.",
+//   "🔥 Showcase your contributions on the hub",
+// ]
+// )}
+{/* <h3 className={styles.hubIntro}> LLM-based apps are programmed in a completely new way.   </h3>
+        <h3 className={`${styles.hubIntro} mb-5`}> We fix all the problems with building apps on top of them, and with using those apps.</h3> */}
