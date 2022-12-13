a5fd7b1cd3b00f97649c20bb1eec4388dbf91ebe
Siddharth Sharma <siddharthsharma0508@gmail.com>
2022-12-13 18:17:24 +0000

    fix: likes API (#71)

--- client/hub/pages/api/v0/[workspaceId]/functions/[functionId]/like.js
+++ client/hub/pages/api/v0/[workspaceId]/functions/[functionId]/like.js
@@ -3,7 +3,7 @@ import { authOptions } from '../../../../auth/[...nextauth]'
 import { unstable_getServerSession } from "next-auth/next"
 
 export default async function handler(req, res) {
-  const { workspaceId, id, mode } = req.query
+  const { workspaceId, functionId, mode } = req.query
   const session = await unstable_getServerSession(req, res, authOptions)
 
   if (req.method !== 'GET') return res.status(500).json({ error: 'Invalid request' });
@@ -19,9 +19,9 @@ export default async function handler(req, res) {
 
   let newLikes = users[0].likes;
   if (mode === 'true') {
-    newLikes.push(parseInt(id));
+    newLikes.push(parseInt(functionId));
   } else {
-    newLikes = newLikes.filter(l => l !== parseInt(id))
+    newLikes = newLikes.filter(l => l !== parseInt(functionId))
   }
 
   // update users
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   let data
   (
     { data, error } = await supabase
-      .rpc('increment_likes', { value: mode === 'true' ? 1 : -1, row_id: id })
+      .rpc('increment_likes', { value: mode === 'true' ? 1 : -1, row_id: functionId })
   )
 
   // return to frontend
