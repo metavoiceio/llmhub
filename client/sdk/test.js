9e7deb1608c916bcaa6eae5000ce490ea08eb183
Vatsal <vatsalaggarwal@gmail.com>
2022-12-13 17:52:22 +0000

    update: node sdk

--- client/sdk/test.js
+++ client/sdk/test.js
@@ -1,8 +1,9 @@
+// Tests the LLMHub SDK without going through npm.
 const LLMHub = require("./main");
 
 const llmhub = new LLMHub("https://www.llmhub.com/2/functions/5/share")
 llmhub.run({
-    input: "This is a test prompt"
+    request: "This is a test prompt"
 }).then(
     (response) => {
         console.log(response);
--- tests/integration_tests/sdk/index.js
+++ tests/integration_tests/sdk/index.js
@@ -1,7 +1,8 @@
+// Tests the LLMHub SDK after going through npm install!
 const LLMHub = require("llmhub");
 
 const llmhub = new LLMHub("https://www.llmhub.com/2/functions/5/share")
-llmhub.run({input: "This is a test prompt"}).then(
+llmhub.run({ description: "This is a test prompt" }).then(
     (response) => {
         console.log(response);
     }
